fbb7882c1abcb5fc94a5a947b50457fbe32af00f
valentin.ivanov <valentin.ivanov@nexusalpha.com>
2018-08-20 13:17:26 +0100

    First commit

---
+++ .history/__tests__/LoginTest_20180814145700.js
@@ -0,0 +1,50 @@
+import 'react-native';
+import React from 'react';
+import Login from '../Login';
+import renderer from 'react-test-renderer';
+// import {shallow} from 'enzyme';
+import * as enzyme from 'enzyme';
+import configureStore from 'redux-mock-store';
+import Adapter from 'enzyme-adapter-react-16';
+
+    const initialState = {
+        username: 'valentin',
+        password: 'bla'
+    };
+
+    const middlewares = [];
+    const mockStore = configureStore(middlewares);
+
+    enzyme.configure({ adapter: new Adapter() });
+
+    describe("Testing Mock Login page", () => {
+        console.log("username", initialState.username, "|password", initialState.password);
+        it("Test render", () => {
+            const testRender = renderer.create(
+            <Login />
+            ).toJSON;
+            expect(testRender).toMatchSnapshot();
+        });
+        it("Test credentials", () =>{
+            const wrapper = enzyme.shallow(
+                <Login />,
+                {context: {store: mockStore(initialState)}},
+            );
+            console.log("Wrapper selector", wrapper.state());
+            // expect(wrapper.state('username')).equal('valentin');
+            expect(wrapper.dive()).toMatchSnapshot();
+        });
+        it("Test states", () =>{
+            const login = enzyme.shallow(
+                <Login username="val" password="123" />
+            );
+            expect(login.state('username')).toEqual('val');
+        });
+        it("Test props", () =>{
+            const username = 'val';
+            const login = enzyme.shallow(
+                <Login username={username} password={"123"} />
+            );
+            expect(login.props('username')).toEqual('val');
+        });
+    });
---
+++ Login.js
@@ -0,0 +1,112 @@
+// import React, {Component} from 'react';
+// import {
+//     View,
+//     Text
+// } from 'react-native';
+// import { FormInput, FormLabel, FormValidationMessage } from 'react-native-elements';
+
+
+// class Login extends Component {
+//     componentWillReceiveProps(){
+//         console.log("inside CWP");
+//         console.log("Props", this.refs.forminput.refs.username);
+//     }
+//     render(){
+//         console.log("Console working");
+//         return(
+//             <View>
+//                 <Text>Login</Text>
+//                 <FormLabel>username:</FormLabel>
+//                 <FormInput
+//                 textInputRef="username"
+//                 placeholder='Input username'
+//                 ref={input => this.input = input}
+//                 />
+//                 <FormLabel>password:</FormLabel>
+//                 <FormInput
+//                 textInputRef="password"
+//                 placeholder='Input password'
+//                 passref={input => this.input = input}
+//                 />
+//                 <FormValidationMessage>This field is required</FormValidationMessage>
+//             </View>
+//         );
+//     }
+//     componentWillUnmount(){
+//         console.log("Form username", this.refs.forminput.refs.username);
+//     }
+// }
+
+// export default Login;
+import React, { Component } from 'react';
+import { Text,TextInput, View, Button, StyleSheet } from 'react-native';
+// import t from 'tcomb-form-native'; // 0.6.15
+
+// Form
+// const Form = t.form.Form;
+
+// // Form model
+// const User = t.struct({
+//   email: t.String,
+//   password: t.String,
+// });
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      username: 'Mock username',
+      password: '123'
+    };
+  }
+  handleSubmit = () => {
+    console.log("username",this.state.username);
+    console.log("username",this.state.password);
+
+    this.props.dispatch({type:'Login', username});
+    // alert(this.);
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+      <Text>Login</Text>
+        {/* <Form ref={c => this._form = c} type={User} /> */}
+        {/* <Form ref="form" type={User} /> */}
+        {/* <Button
+          title="Login"
+          onPress={this.handleSubmit}
+        /> */}
+        <Text>username. Current username - {this.props.username}</Text>
+         <TextInput
+          {...this.props}
+          editable = {true}
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(username) => this.setState({username})}
+          value={this.state.username}
+        />
+        <Text>password</Text>
+        <TextInput
+          {...this.props}
+          editable = {true}
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(password) => this.setState({password})}
+          value={this.state.password}
+        />
+        <Button
+          title="Login"
+          onPress={this.handleSubmit}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
+  },
+});
